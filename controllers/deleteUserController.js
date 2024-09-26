4e4f67bd2e694b0689a004ac768a3737b7c2060a
Mohd Zama <zama9170@gmail.com>
2024-09-26 15:21:29 +0530

    correction in delete controller

--- controllers/deleteUserController.js
+++ controllers/deleteUserController.js
@@ -1,10 +1,10 @@
 const pool = require('../config/db.js');
 
 const deleteUser = async (req, res) => {
-    const { user_id } = req.headers;
+    const userId = parseInt(req.headers.user_id);
     try {
         await pool.query(
-            `DELETE FROM users WHERE user_id = $1`, [user_id]
+            `DELETE FROM users WHERE user_id = $1`, [userId]
         );
         res.status(200).json({ message: 'user deleted successfully' });
     } catch (dbError) {
